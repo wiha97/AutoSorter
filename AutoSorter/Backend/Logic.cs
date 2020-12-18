0650c07b9bd522e6bc1f6ff59cc027414ec2e993
w <nvz2z0k9wzzz@opayq.com>
2020-12-18 14:29:56 +0100

    Add project files.

---
+++ AutoSorter/Backend/Logic.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Threading;
+using System.IO;
+
+namespace AutoSorter.Backend
+{
+    public class Logic
+    {
+        private string proc;
+        private string dir;
+        private List<string> procs;
+        private List<string> aProcs;
+        private DispatcherTimer timer;
+
+        public string Proc
+        {
+            get { return proc; }
+            set { proc = value; }
+        }
+        public string Dir
+        {
+            get { return dir; }
+            set { dir = value; }
+        }
+        public void GetProcName(string name)
+        {
+            proc = name;
+        }
+        public void GetDirName(string dName)
+        {
+            dir = dName;
+        }
+        public List<string> Procs
+        {
+            get { return procs; }
+            set { procs = value; }
+        }
+        public DispatcherTimer Timer
+        {
+            get { return timer; }
+        }
+        public void FillList()
+        {
+            procs = File.ReadAllLines(@"config\apps.txt").ToList();
+        }
+        public void AddToList(string app)
+        {
+            procs.Add(app);
+            File.AppendAllText(@"config\apps.txt", app);
+        }
+        public List<string> AProcs
+        {
+            get { return aProcs; }
+            set { aProcs = value; }
+        }
+        public void GetProcs()
+        {
+            aProcs = new List<string>();
+            //aProcs = Process.GetProcesses().ToList();
+            foreach(Process process in Process.GetProcesses().OrderBy(p => p.ProcessName))
+            {
+                aProcs.Add(process.ProcessName);
+            }
+        }
+        public bool GetProcess()
+        {
+            bool bl = false;
+            try
+            {
+                //string procName = "";
+                //var processes = Process.GetProcesses();
+                //foreach (Process process in processes)
+                //{
+                //    procName = process.ProcessName;
+                //    if ((procName == proc) || (File.ReadAllLines(@"config\apps.txt").ToList().Contains(procName)))
+                //    {
+                //        proc = procName;
+                //        bl = true;
+                //        break;
+                //    }
+                //}
+                foreach (string app in procs)
+                {
+                    if (Process.GetProcessesByName(app).Length > 0)
+                    {
+                        proc = app;
+                        bl = true;
+                        break;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+            return bl;
+        }
+        public void DirTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(TickTick);
+            timer.Interval = new TimeSpan(0, 0, 5);
+            //timer.Start();
+        }
+
+        async void TickTick(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                CheckDir();
+            });
+        }
+
+        public void CheckDir()
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(dir);
+                if (files.Count() > 0)
+                {
+                    if (!Directory.Exists(dir + @"\" + proc))
+                    {
+                        Directory.CreateDirectory(dir + @"\" + proc);
+                    }
+                    foreach (string file in files)
+                    {
+                        File.Move(file, dir + @"\" + proc + @"\" + Path.GetFileName(file));
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ AutoSorter/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using AutoSorter.Backend;
+
+namespace AutoSorter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Logic log;
+        private DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Feeder();
+            log = new Logic();
+            //log.GetProcName("notepad");
+            log.GetDirName(srcBox.Text);
+            log.FillList();
+            Ticker();
+            log.DirTimer();
+            //Processes();
+        }
+
+        void Ticker()
+        {
+            try
+            {
+                timer = new DispatcherTimer();
+                timer.Tick += new EventHandler(Timer_Tick);
+                timer.Interval = new TimeSpan(0, 0, 1);
+                //timer.Start();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        private async void Timer_Tick(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                //log.GetProcess("notepad");
+                this.Dispatcher.Invoke(() =>
+                {
+                    if (all.IsChecked == true)
+                    {
+                        log.GetProcs();
+                        Processes(log.AProcs);
+                    }
+                    if (log.GetProcess())
+                    {
+                        label.Content = string.Format("Saving to {0}", log.Proc);
+                    }
+                    else
+                    {
+                        label.Content = "No saved found, last process: " + log.Proc;
+                    }
+                });
+            });
+        }
+
+        void Processes(List<string> procs)
+        {
+            ListView list = new ListView();
+            ScrollViewer scroll = new ScrollViewer();
+            stacky.Children.Clear();
+            list.Background = new SolidColorBrush(Colors.DarkGray);
+            list.ItemsSource = procs;
+            list.MouseDoubleClick += new MouseButtonEventHandler(listDbl);
+            stacky.Children.Add(list);
+        }
+
+        private void listDbl(object sender, EventArgs e)
+        {
+            ListView list = sender as ListView;
+            procBox.Text = list.SelectedItem.ToString();
+        }
+
+        private List<string> Procs()
+        {
+            List<string> procs = new List<string>();
+            foreach (Process proc in Process.GetProcesses().OrderBy(p => p.ProcessName))
+            {
+                procs.Add(proc.ProcessName);
+            }
+            return procs;
+        }
+
+        public void CheckProcess()
+        {
+            Logic log = new Logic();
+            //log.CheckProcess("notepad");
+        }
+
+        public void Feeder()
+        {
+            srcBox.Text = File.ReadLines(@"config\path.txt").First();
+
+        }
+
+        private void procBtn_Click(object sender, RoutedEventArgs e)
+        {
+            log.AddToList(procBox.Text);
+        }
+
+        private void autoBox_Checked(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void autoBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void autoFile_Checked(object sender, RoutedEventArgs e)
+        {
+            log.Timer.Stop();
+        }
+
+        private void autoFile_Unchecked(object sender, RoutedEventArgs e)
+        {
+            log.Timer.Stop();
+        }
+
+        private void getProc_Click(object sender, RoutedEventArgs e)
+        {
+            log.GetProcName(procBox.Text);
+            label.Content = "Saving to " + log.Proc;
+        }
+
+        private void saved_Checked(object sender, RoutedEventArgs e)
+        {
+            Processes(log.Procs);
+        }
+    }
+}
